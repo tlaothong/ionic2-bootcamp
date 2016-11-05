ff0ceb2b948fa31b509e1db3f1ee0144f492ea57
Teerachai <teerachai@perfenterprise.com>
2016-11-05 15:28:39 +0700

    Init demo with firstdem.

---
+++ demos/firstdem/src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { AlertController, NavController } from 'ionic-angular';
+// import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+}
