d0733960ebf55d93c2914e03a084e22bf609fe29
Teerachai Laothong <tlaothong@gmail.com>
2016-11-19 15:46:51 +0700

    workshop5 http

--- demos/workshop5/src/pages/home/home.ts
+++ demos/workshop5/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public items: any = [];
 
+  constructor(public navCtrl: NavController, private http: Http) {
+
+  }
+
+  public  ionViewDidEnter() {
+    var url = 'https://raw.githubusercontent.com/tlaothong/ionic2-bootcamp/master/albums.json';
+    this.http.get(url).subscribe(rsp => {
+      this.items = rsp.json();
+    });
   }
 
 }
